aa43f4a4d184067d40ac913ce554c001574e29e6
Richard You <richardyou94@gmail.com>
2026-08-21 09:37:19 +0900

    adjust file size for open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
     siteName: "Father’s House Church",
     images: [
       {
-        url: "/opengraph-image.png",
+        url: "/opengraph-image.jpg",
         width: 1200,
         height: 630,
         alt: "Father’s House Church",
